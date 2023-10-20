35ad54667fdfeba411c9bbf8026c7c1bc6b132b8
claudia-bianchini <132372907+claudia-bianchini@users.noreply.github.com>
2023-10-20 17:14:25 +0200

    histogram_overlap

--- Project/histogram_ok.py
+++ Project/histogram_ok.py
@@ -1,6 +1,7 @@
 import os.path
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib import colors as mcols
 
 # Function to assign a season
 def assign_seasons(dataframe):
@@ -99,6 +100,15 @@ south_mean_temp_TS = south_data_year['TS'].mean()
 print(f'Mean in the north: ', len(north_mean_temp_TS))
 print(f'Mean in the south: ', len(south_mean_temp_TS))
 
+# Extract the data we want to plot
+
+north_TS_winter = north_data_year.get_group('2015')[north_data_year.get_group('2015')['season'] == 'Winter']['TS']
+south_TS_winter = south_data_year.get_group('2015')[south_data_year.get_group('2015')['season'] == 'Winter']['TS']
+
+east_TS_winter = east_data_year.get_group('2015')[east_data_year.get_group('2015')['season'] == 'Winter']['TS']
+west_TS_winter = west_data_year.get_group('2015')[west_data_year.get_group('2015')['season'] == 'Winter']['TS']
+
+'''
 # Create histograms to visualize the TS variation between north and south
 fig, axes = plt.subplots(1, 2, figsize=(10, 5))
 
@@ -120,8 +130,8 @@ plt.show()
 # Create histograms to visualize the TS variation between north and south
 fig, axes = plt.subplots(1, 2, figsize=(10, 5))
 
-north_TS_winter = north_data_year.get_group('2015')[north_data_year.get_group('2015')['season'] == 'Winter']['TS']
-south_TS_winter = south_data_year.get_group('2015')[south_data_year.get_group('2015')['season'] == 'Winter']['TS']
+#north_TS_winter = north_data_year.get_group('2015')[north_data_year.get_group('2015')['season'] == 'Winter']['TS']
+#south_TS_winter = south_data_year.get_group('2015')[south_data_year.get_group('2015')['season'] == 'Winter']['TS']
 #north_TS_winter = north_data_year[north_data_year['season' == 'winter']].get_group('2016')['TS']  # Change the year to the one you're interested in
 #south_TS_winter = south_data_year.get_group('2016')['TS']  # Change the year to the one you're interested in
 
@@ -133,7 +143,7 @@ axes[1].set_title('South Region - TS')
 
 for ax in axes:
     ax.set_xlabel('TS')
-    ax.set_ylabel('Frequency')
+    ax.set_ylabel('Events')
 
 fig.suptitle('Year TS Variation in North vs South Regions WINTER')
 
@@ -144,19 +154,60 @@ fig, axes = plt.subplots(1, 2, figsize=(10, 5))
 
 
 
-east_TS_winter = east_data_year.get_group('2015')[east_data_year.get_group('2015')['season'] == 'Winter']['TS']
-west_TS_winter = west_data_year.get_group('2015')[west_data_year.get_group('2015')['season'] == 'Winter']['TS']
+#east_TS_winter = east_data_year.get_group('2015')[east_data_year.get_group('2015')['season'] == 'Winter']['TS']
+#west_TS_winter = west_data_year.get_group('2015')[west_data_year.get_group('2015')['season'] == 'Winter']['TS']
 
-axes[0].hist(east_TS_winter, bins=20, color='blue', edgecolor='black', alpha=0.7)
+axes[0].hist(east_TS_winter, bins=100, color='blue', edgecolor='black', alpha=0.7)
 axes[0].set_title('East Region - TS')
 
-axes[1].hist(west_TS_winter, bins=20, color='green', edgecolor='black', alpha=0.7)
+axes[1].hist(west_TS_winter, bins=100, color='green', edgecolor='black', alpha=0.7)
 axes[1].set_title('West Region - TS')
 
 for ax in axes:
     ax.set_xlabel('TS')
-    ax.set_ylabel('Frequency')
+    ax.set_ylabel('Events')
 
 fig.suptitle('Year TS Variation in East vs South Regions WINTER')
 
 plt.show()
+'''
+
+#Try to plot the histogram in one plot 
+common_plot_params = {
+    'density': True,
+    #'alpha': 0.6,
+    #'range': (0, 1e4),
+    'bins': 100,
+    'histtype': 'stepfilled'
+    
+}
+
+fig, ax = plt.subplots()
+
+ax.hist(north_TS_winter, label = 'North', color=mcols.to_rgba('tab:blue', 0.2), edgecolor='tab:blue', **common_plot_params)
+ax.hist(south_TS_winter, label = 'South',color=mcols.to_rgba('tab:orange', 0.2), edgecolor='tab:orange', **common_plot_params)
+
+#for ax in axes:
+ax.set_xlabel('TS')
+ax.set_ylabel('Events')
+    
+fig.suptitle('Year TS Variation in North vs South Regions WINTER')
+ax.legend(frameon=False, loc='upper left')
+
+plt.show()
+
+
+fig, ax = plt.subplots()
+
+ax.hist(east_TS_winter, label = 'East', color=mcols.to_rgba('tab:purple', 0.2), edgecolor='tab:purple', **common_plot_params)
+ax.hist(west_TS_winter, label = 'West', color=mcols.to_rgba('tab:olive', 0.2), edgecolor='tab:olive', **common_plot_params)
+
+#for ax in axes:
+ax.set_xlabel('TS')
+ax.set_ylabel('Events')
+
+fig.suptitle('Year TS Variation in East vs West Regions WINTER')
+ax.legend(frameon=False, loc='upper left')
+
+plt.show()
+
